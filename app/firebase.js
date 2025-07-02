5281f63dfcac973a5631ad70209ab9470a1d34b1
Çağdaş Şahin <cagdassahin@mac.local>
2025-07-02 23:38:58 +0300

    Tüm değişiklikler commitlendi, geçmişten anahtar temizlenecek

--- app/firebase.js
+++ app/firebase.js
@@ -4,19 +4,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } from '@env';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCiueW0pDv0szqnnO0IUUm2OPY5lfjTl-k",
-  authDomain: "unicampus-fb482.firebaseapp.com",
-  projectId: "unicampus-fb482",
-  storageBucket: "unicampus-fb482.firebasestorage.app",
-  messagingSenderId: "957984947371",
-  appId: "1:957984947371:web:491f3db77a1cacdb2f4bee",
-  measurementId: "G-3ZW1P5MC2R"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
 // Initialize Firebase
